184c9c5d6293f332d62b276fab3a16a776f5e69b
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-11 08:31:34 +0700

    Create account activation functionality

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('./../models/User');
 const sendMail = require('./../utils/sendMail');
 const { generateActivationToken } = require('./../utils/generateToken');
@@ -17,10 +18,11 @@ const authCtrl = {
 
       const findAccount = await User.findOne({account});
       if (findAccount) {
-        if (checkEmail(findAccount))
+        if (checkEmail(findAccount.account)) {
           return res.status(400).json({msg: 'Email has been used before.'});
-        else if (checkPhone(findAccount))
+        } else if (checkPhone(findAccount.account)) {
           return res.status(400).json({msg: 'Phone number has been used before.'});
+        }
       }
 
       const passwordHash = await bcrypt.hash(password, 12);
@@ -40,6 +42,29 @@ const authCtrl = {
         sendSms(account, url);
         return res.status(200).json({msg: 'Account activation link has been sent to your phone number.'});
       }
+    } catch (err) {
+      return res.status(500).json({msg: err.message});
+    }
+  },
+  activate: async(req, res) => {
+    try {
+      const {activation_token} = req.body;
+
+      if (!activation_token)
+        return res.status(400).json({msg: 'Please provide the activation token to activate your account.'});
+
+      const decoded = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET);
+      if (!decoded)
+        return res.status(403).json({msg: 'Invalid activation token'});
+
+      const user = await User.findOne({account: decoded.account});
+      if (user)
+        return res.status(400).json({msg: 'Account already exist'});
+
+      const newUser = new User(decoded);
+      await newUser.save();
+
+      res.status(200).json({msg: 'Your account has been activated successfully.'});
     } catch (err) {
       return res.status(500).json({msg: err.message});
     }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const authCtrl = require('./../controllers/authCtrl');
 
 router.post('/register', authCtrl.register);
+router.post('/activate', authCtrl.activate);
 
 module.exports = router;
