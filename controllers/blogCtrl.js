93febb99b9a1ae9444e8940041ddc552ed6ad67d
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-12 16:46:03 +0700

    Create getHomeBlogs functionality

--- controllers/blogCtrl.js
+++ controllers/blogCtrl.js
@@ -1,6 +1,70 @@
 const Blog = require('./../models/Blog');
+const mongoose = require('mongoose');
 
 const blogCtrl = {
+  getHomeBlogs: async(req, res) => {
+    const categoryQuery = [];
+    for (let i = 0; i < req.query.category?.length; i++) {
+      categoryQuery.push(mongoose.Types.ObjectId(req.query.category[i]));
+    }
+
+    const aggregation = [
+      {
+        $lookup: {
+          "from": "users",
+          "let": {user_id: "$user"},
+          "pipeline": [
+            {$match: {$expr: {$eq: ["$_id", "$$user_id"]}}},
+            {$project: {password: 0, role: 0, type: 0, createdAt: 0, updatedAt: 0}}
+          ],
+          "as": "user"
+        }
+      },
+      {$unwind: "$user"},
+      {
+        $lookup: {
+          "from": "categories",
+          "localField": "category",
+          "foreignField": "_id",
+          "as": "category"
+        }
+      },
+      {$unwind: "$category"},
+      {
+        $group: {
+          _id: "$category._id",
+          name: { $first: "$category.name" },
+          blogs: { $push: "$$ROOT" },
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          blogs: {
+            $slice: ['$blogs', 0, 4]
+          },
+          name: 1,
+          count: 1
+        }
+      }
+    ];
+    
+    if (categoryQuery.length !== 0) {
+      aggregation.unshift({
+        $match: {
+          category: {$in: categoryQuery}
+        }
+      });
+    }
+
+    try {
+      const blogs = await Blog.aggregate(aggregation);
+
+      res.status(200).json({blogs});
+    } catch (err) {
+      return res.status(500).json({msg: err.message});
+    }
+  },
   createBlog: async(req, res) => {
     try {
       const {title, content, description, thumbnail, category} = req.body;
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,6 +3,7 @@ const blogCtrl = require('./../controllers/blogCtrl');
 const { isAuthenticated } = require('./../middlewares/auth');
 
 router.route('/')
+  .get(blogCtrl.getHomeBlogs)
   .post(isAuthenticated, blogCtrl.createBlog);
 
 module.exports = router;
