d3156a096cd2b27216aaf26ef9cb74906f3c8b25
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-12 14:59:19 +0700

    Prevent non-admin role to access category page

--- client/src/pages/category.js
+++ client/src/pages/category.js
@@ -1,8 +1,13 @@
 import { Grid } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import NotFound from './../components/global/NotFound';
 import CategoryForm from './../components/category/CategoryForm';
 import CategoryTable from './../components/category/CategoryTable';
 
 const Category = () => {
+  const {auth} = useSelector(state => state);
+
+  if (auth.user?.role !== 'admin') return <NotFound />
   return (
     <Grid
       p={{ base: '30px 35px', md: '30px 100px' }}
