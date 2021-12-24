c15e1af85a0b329fa15cc898d44b75148efd1726
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-24 23:42:34 +0700

    Create spinner component

---
+++ client/src/components/global/Spinner.js
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className='spinner' />
+    </div>
+  );
+}
+
+export default Spinner;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,6 +5,7 @@ import { getHomeBlogs } from './../redux/actions/blogActions';
 import ArticleContainer from './../components/home/ArticleContainer';
 import Header from './../components/home/Header';
 import Filter from './../components/home/Filter';
+import Spinner from './../components/global/Spinner';
 
 const Home = () => {
   const [filter, setFilter] = useState([]);
@@ -27,7 +28,7 @@ const Home = () => {
       <div className='home'>
         {
           alert.loading
-          ? 'Loading ...'
+          ? <Spinner />
           : (
             <>
               {
@@ -50,35 +51,6 @@ const Home = () => {
           )
         }
       </div>
-      {/* <Box
-        p={{ base: '15px 35px', md: '15px 100px' }}
-      >
-        {
-          alert.loading
-          ? (
-            <Center>
-              <Spinner size='xl' />
-            </Center>
-          )
-          : (
-            <>
-              {
-                blogCategory.map(cat => (
-                  <Box key={cat._id}>
-                    <Flex alignItems='center'>
-                      <Heading fontSize='27px'>{cat.name}</Heading>
-                      <Spacer />
-                      {cat.count > 4 && <Link to={`/blogs/${cat.name}`}>See more</Link>}
-                    </Flex>
-                    <Divider bg='white' m='20px 0' />
-                    <ArticleContainer blogs={cat.blogs} />
-                  </Box>
-                ))
-              }
-            </>
-          )
-        }
-      </Box> */}
     </div>
   )
 }
