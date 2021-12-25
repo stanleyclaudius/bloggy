76fb752fc8092e2499a5e56f0470b52e301bbcd7
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-25 23:18:36 +0700

    Remove AvatarComment component

--- client/src/components/comment/AvatarComment.js
+++
@@ -1,9 +0,0 @@
-import { Avatar } from '@chakra-ui/react';
-
-const AvatarComment = ({avatar}) => {
-  return (
-    <Avatar size='md' name='Avatar' src={avatar} />
-  );
-}
-
-export default AvatarComment;
