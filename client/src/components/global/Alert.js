26ccf294342184d4660c277de7fe8e8cce65b394
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-25 23:48:56 +0700

    Restyle Alert component

--- client/src/components/global/Alert.js
+++ client/src/components/global/Alert.js
@@ -1,36 +1,21 @@
+import { useAlert } from 'react-alert';
 import { useEffect } from 'react';
-import { Box, createStandaloneToast } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 
 const Alert = () => {
-  const toast = createStandaloneToast();
-
+  const toast = useAlert();
   const { alert } = useSelector(state => state);
 
   useEffect(() => {
     if (alert.success) {
-      toast({
-        position: 'top-right',
-        title: 'Success.',
-        description: `${alert.success}`,
-        status: 'success',
-        duration: 3000,
-        isClosable: true
-      });
+      toast.success(alert.success);
     } else if (alert.errors) {
-      toast({
-        position: 'top-right',
-        title: 'Failed.',
-        description: `${alert.errors}`,
-        status: 'error',
-        duration: 3000,
-        isClosable: true
-      });
+      toast.error(alert.errors);
     }
-  }, [alert, toast]);
+  }, [alert]);
 
   return (
-    <Box></Box>
+    <div></div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import App from './App';
 import ReduxProvider from './redux/store';
 import './styles/css/main.css';
 
+const options = {
+  position: positions.TOP_RIGHT,
+  timeout: 2000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <ReduxProvider>
-      <App />
-    </ReduxProvider>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <ReduxProvider>
+        <App />
+      </ReduxProvider>
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
