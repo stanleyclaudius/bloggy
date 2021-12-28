4789aa1d9ffb534d7b756e8255c0c72ba24df2e8
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-12-28 15:13:52 +0700

    Fix database connection variable

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
-  await mongoose.connect(process.env.MONGODB_url, err => {
+  await mongoose.connect(process.env.MONGODB_URL, err => {
     if (err) throw err;
     console.log('Successfully connected to MongoDB.');
   })
